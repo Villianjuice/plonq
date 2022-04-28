b39ac3e0182ecbc21c3ac6833c56880168854e15
Aslan <tavorktar@gmail.com>
2022-04-29 03:16:35 +0600

    err

--- src/views/Plonq500.vue
+++ src/views/Plonq500.vue
@@ -3,14 +3,17 @@
     <Product2/>
     <Product3/>
     <Product4/>
+    <Product5/>
+
 </template>
 <script>
   import Product1 from '../components/plonq500/product/Product-1.vue'
   import Product2 from '../components/plonq500/product/Product-2.vue'
   import Product3 from '../components/plonq500/product/Product-3.vue'
   import Product4 from '../components/plonq500/product/Product-4.vue'
+  import Product5 from '../components/plonq500/product/Product-5.vue'
 export default {
-    components: {Product1, Product2, Product3, Product4},
+    components: {Product1, Product2, Product3, Product4, Product5},
 }
 </script>
 <style lang="scss">
